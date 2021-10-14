f59625f8c130c6b17170b67c7c0eba7b78fa1fee
Agil Kerimov <ahkerimov@gmail.com>
2021-10-14 13:58:57 +0400

    v 1.1.0 multipliers

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function scheduleMaker({
   payFirst = false,
   rounded = 5,
   initialPayment = 0,
+  multipliers = [],
 }) {
   function startFromDate() {
     const contractDate = new Date(startDate);
@@ -33,6 +34,7 @@ function scheduleMaker({
           pay: initialPayment,
           debt: amount,
           date: new Date(),
+          doNotCount: true,
         },
       ]
     : [];
@@ -46,21 +48,26 @@ function scheduleMaker({
       date.setDate(0);
     }
 
-    const halfMonth = (new Date(startDate) - new Date(date)) / 86400000 >= 15;
+    const daysPast = Math.max(0, (new Date(startDate) - new Date(date)) / 86400000);
+
+    const [multiplier = 1] = multipliers.find(f => f[1] <= daysPast && daysPast <= f[2]) || [1];
 
     const prevPays = acc.reduce((c, i) => c + i.pay, 0);
-    const prevMaxPay = acc.reduce((c, i) => (c > i.pay ? c : i.pay), 0);
-    const mustPay = amount + (initialPayment ?? 0) - prevPays;
+    const payMaxDiff = acc.reduce((c, i) => c + i.payMaxDiff, 0);
+    const prevMaxPay = acc.filter(f => !f.doNotCount).reduce((c, i) => (c > i.pay ? c : i.pay), 0);
+    const mustPay = amount + (initialPayment ? initialPayment : 0) - prevPays - payMaxDiff;
 
     const monthly = Math.max(prevMaxPay, mustPay / (periods - index));
     const annuity = round(monthly, rounded);
 
-    const pay = round((mustPay > annuity ? annuity : mustPay) / (halfMonth ? 2 : 1), rounded);
-    const debt = mustPay - Math.min(pay, mustPay);
+    const payMax = round(mustPay > annuity ? annuity : mustPay, rounded);
+    const pay = round(payMax * multiplier, rounded);
+    const debt = mustPay - Math.min(pay, mustPay) - payMax + pay;
     if (pay > 0) {
       acc.push({
         id: index + indexIncrementer,
         pay: Math.min(pay, mustPay),
+        payMaxDiff: pay !== payMax ? payMax - pay : 0,
         debt,
         date: new Date(Math.max(Number(new Date(startDate)), Number(new Date(date)))),
       });
